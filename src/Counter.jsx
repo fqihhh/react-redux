f6a522b3827607779538176796dc2e548c009212
fqihhh <arsyad2104@student.abudzar.sch.id>
2026-04-09 11:09:07 +0700

    done

---
+++ src/Counter.jsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  getCounter,
+  getDoubleCounter,
+} from "./counterSlice.js";
+
+export default function Counter() {
+  const counter = useSelector(state => state.counter);
+  const double = useSelector(getDoubleCounter);
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(increment());
+  }
+
+  function handleDecrement() {
+    dispatch(decrement());
+  }
+
+  const doubleCounter = useSelector(getDoubleCounter);
+  const tripleCounter = useSelector(state => getCounter(state, 3));
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <h1>Double Counter: {doubleCounter}</h1>
+      <h1>Triple Counter: {tripleCounter}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={() => dispatch(increment(2))}>Increment +2</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <button onClick={() => dispatch(decrement(2))}>Decrement -2</button>
+    </div>
+  );
+}
